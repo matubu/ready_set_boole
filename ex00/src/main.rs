c5ba16ccdc2b49619a858fc2fa0792be60e737c8
Mathias Berger--fruhinsholz <throw_new_Error@protonmail.com>
2022-07-21 22:29:09 +0200

    ex00 + ex01

---
+++ ex00/src/main.rs
@@ -0,0 +1,29 @@
+fn	adder(a: u32, b: u32) -> u32 {
+	let	mut carry: bool = false;
+	let mut result: u32 = a ^ b;
+
+	for i in 0..32 {
+		result ^= (carry as u32) << i;
+		carry = (1
+			<< ((a >> i) & 1)
+			<< ((b >> i) & 1)
+			<< (carry as u32)
+		) > 2;
+	}
+
+	result
+}
+
+fn	test(a: u32, b: u32) {
+	println!("{} == {}", a + b, adder(a, b));
+}
+
+fn	main() {
+	test(0, 0);
+	test(1, 1);
+	test(0, 1);
+	test(5, 5);
+	test(9874, 12896);
+	test(75, 35);
+	test(32, 32);
+}
