f866b44f4bab60f440379066091cd7e4e3e25068
Mathias Berger--fruhinsholz <throw_new_Error@protonmail.com>
2022-07-23 01:02:43 +0200

    cleanup ex05

--- ex05/src/main.rs
+++ ex05/src/main.rs
@@ -1,34 +1,31 @@
 use std::iter::Rev;
 use std::str::Chars;
 
-fn	negation_it(it: &mut Rev<Chars>, negate: bool) -> String {
-	match it.next() {
-		Some('!') => format!("{}", negation_it(it, !negate)),
-		Some('&') => format!("{1}{0}{op}", negation_it(it, negate), negation_it(it, negate),
-			op=if negate { "|" } else { "&" }),
-		Some('|') => format!("{1}{0}{op}", negation_it(it, negate), negation_it(it, negate),
-			op=if negate { "&" } else { "|" }),
-		Some('^') => format!("{1}{0}{op}", negation_it(it, negate), negation_it(it, !negate),
-			op=if negate { "|" } else { "&" }),
-		Some('>') => format!("{1}{0}{op}", negation_it(it, negate), negation_it(it, !negate),
-			op=if negate { "&" } else { "|" }),
-		Some('=') => {
-			let copy = &mut (*it).clone();
-			format!("{1}{0}{and}{2}{3}{and}{or}",
-				negation_it(it, negate), negation_it(it, negate),
-				negation_it(copy, !negate), negation_it(copy, !negate),
-				and=if negate { "|" } else { "&" },
-				or=if negate { "&" } else { "|" }
-			)
-		},
-		Some(c @ 'A'..='Z') => format!("{}{op}", c, op=if negate { "!" } else { "" }),
-		None => panic!("\x1B[91merror\x1B[0m end of string"),
-		_ => panic!("\x1B[91merror\x1B[0m invalid char")
-	}
-}
-
 fn negation_normal_form(formula: &str) -> String {
-	negation_it(&mut formula.chars().rev(), false)
+	fn	fixup(it: &mut Rev<Chars>, neg: bool) -> String {
+		let and = if neg { "|" } else { "&" };
+		let or = if neg { "&" } else { "|" };
+
+		match it.next() {
+			Some('!') => fixup(it, !neg),
+			Some('&') => format!("{1}{0}{and}", fixup(it, neg), fixup(it, neg)),
+			Some('|') => format!("{1}{0}{or}", fixup(it, neg), fixup(it, neg)),
+			Some('^') => format!("{1}{0}{and}", fixup(it, neg), fixup(it, !neg)),
+			Some('>') => format!("{1}{0}{or}", fixup(it, neg), fixup(it, !neg)),
+			Some('=') => {
+				let copy_it = &mut (*it).clone();
+				format!("{1}{0}{and}{2}{3}{and}{or}",
+					fixup(it, neg), fixup(it, neg),
+					fixup(copy_it, !neg), fixup(copy_it, !neg)
+				)
+			},
+			Some(c @ 'A'..='Z') => format!("{}{op}", c, op=if neg { "!" } else { "" }),
+			None => panic!("\x1B[91merror\x1B[0m end of string"),
+			_ => panic!("\x1B[91merror\x1B[0m invalid char")
+		}
+	}
+
+	fixup(&mut formula.chars().rev(), false)
 }
 
 fn	test(formula: &str, expected: &str) {
