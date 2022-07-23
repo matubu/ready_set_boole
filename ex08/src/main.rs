f8b3424a595feb1ac942524204556309eb96dd04
Mathias Berger--fruhinsholz <throw_new_Error@protonmail.com>
2022-07-23 19:43:00 +0200

    ex08

---
+++ ex08/src/main.rs
@@ -0,0 +1,29 @@
+fn powerset(set: &[i32]) -> Vec<Vec<i32>> {
+	let mut powerset: Vec<Vec<i32>> = Vec::new();
+
+	let permutations = 1 << set.len();
+	for i in 0..permutations {
+		let mut j = 0;
+		let mut permutation: Vec<i32> = Vec::new();
+		for value in set {
+			if (i >> j) % 2 == 1 {
+				permutation.push(*value);
+			}
+			j += 1;
+		}
+		powerset.push(permutation);
+	}
+	
+	powerset
+}
+
+fn	test(set: &[i32]) {
+	println!("{:?} => {:?}", set, powerset(set));
+}
+
+fn	main() {
+	test(&[1]);
+	test(&[1, 2]);
+	test(&[1, 2, 3]);
+	test(&[1, 2, 3, 4]);
+}
